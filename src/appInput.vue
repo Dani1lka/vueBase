527119fb616ee6c88035879d35d46b959ebcc483
dani1lka <ostapchuk.d.03@mail.ru>
2023-01-24 17:09:28 +0200

    add

---
+++ src/appInput.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="form-control"
+    :class="{invalid:error}">
+      <label :for="id">{{label}}</label>
+      <input type="text" 
+      :id="id" 
+
+      :placeholder="placeholder" 
+      :value="modelValue" 
+      @input="change">
+      <small style="fontSize:20px" v-if="error"><b>{{error}}</b></small>
+    </div>
+</template>
+
+<script>
+    export default{
+      name: 'appInput',
+      data(){
+        return{
+          id: 'input'+Math.random(),
+        }
+      },
+      props:{
+        label: String,
+        error: String,
+        placeholder: String,
+        modelValue: String,
+      },
+      emits:['update:modelValue'],
+      methods:{
+        change(event){
+          this.$emit('update:modelValue',event.target.value)
+        }
+      },
+    }
+</script>
